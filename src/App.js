5acee38ba0fdbb73430f02395d3da2726469dcac
Freja <frej400d@stud.kea.dk>
2022-05-11 17:35:27 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Artists from "./routes/Artists";
+import Schedule from "./routes/Schedule";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <aside>
+        <nav>
+          <Link to="/Artists">Artists</Link> |{" "}
+          <Link to="/Schedule">Schedule</Link>
+        </nav>
+        <header>
+          {" "}
+          <Link to="/">Home</Link>
+        </header>
+      </aside>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="artists" element={<Artists />} />
+        <Route path="schedule" element={<Schedule />} />
+        {/* <Route path="artists/:id" element={<Schedule />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
