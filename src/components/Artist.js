736f1f50dad83d1e77ca5ff841e62b63eb07ee24
Freja <frej400d@stud.kea.dk>
2022-05-11 21:54:35 +0200

    images in

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,9 +1,33 @@
 //import { Link } from "react-router-dom";
 
 export default function Artist(props) {
+  /*   function getImg() {
+    const herokuAPI = "https://hwaiting.herokuapp.com/logos/";
+    let bandImg = props.artist.logo;
+    console.log("bandimg", bandImg);
+    let logoUrl;
+
+    if (props.artist.logo.includes("https")) {
+      return props.artist.logo;
+    } else {
+      logoUrl = herokuAPI.concat(bandImg);
+
+      return logoUrl;
+    }
+  } */
+
   return (
-    <article className="Artist">
-      <h2>{props.artist.name}</h2>
+    <article className="artist-card">
+      <h3 className="artist-title">{props.artist.name}</h3>
+      <img
+        className="artist-card-img"
+        src={
+          !props.artist.logo.includes("http")
+            ? `https://hwaiting.herokuapp.com/logos/${props.artist.logo}`
+            : props.artist.logo
+        }
+        alt="artist img"
+      />
 
       {/*    <Link to={`/artists/${props.product.name}`}>Read More</Link>  */}
     </article>
