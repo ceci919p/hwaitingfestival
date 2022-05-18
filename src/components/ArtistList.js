edeaaf07d362d6275a7f3c8ee52365e9184b9a81
Freja <frej400d@stud.kea.dk>
2022-05-18 18:30:27 +0200

    day filter yay

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -1,12 +1,26 @@
 import Artist from "./Artist";
 
-export default function ArtistList({ artists }) {
-  //console.log(artists);
-  return (
-    <section id="Artistlist">
-      {artists.map((a) => (
-        <Artist key={a.name} artist={a} />
-      ))}
-    </section>
-  );
+export default function ArtistList({ schedule, artists, dayFilter }) {
+  //console.log(schedule[0].day);
+  let filteredSchedule = [];
+  schedule.forEach((day) => {
+    console.log(day.day);
+    if (day.day === dayFilter || dayFilter === "all") {
+      filteredSchedule.push(day);
+    }
+  });
+  const filterData = [];
+  artists.forEach((band) => {
+    filteredSchedule.forEach((schedule) => {
+      if (band.name.includes(schedule.act)) {
+        filterData.push(band);
+      }
+    });
+  });
+
+  //const filte
+
+  const artistsList = filterData.map((a) => <Artist key={a.name} artist={a} />);
+
+  return <section id="Artistlist">{artistsList}</section>;
 }
--- src/routes/Artists.js
+++ src/routes/Artists.js
@@ -1,20 +1,11 @@
 import { useState, useEffect } from "react";
 import ArtistList from "../components/ArtistList";
 
-/* function makeList(artists, schedule) {
-//console.log("artists:", artists)
-//console.log("schedule:", schedule)
-return artists.map((a) => {
-  //const [name, members, genre, logoCredits, logo, bio] = artists;
-  a.day = a.name;
-  return a;
-});
-
-} */
-
 function Artists() {
   const [artists, setArtists] = useState([]);
   const [schedule, setSchedule] = useState([]);
+  const [dayFilter, setDayFilter] = useState("all");
+  const [genreFilter, setGenreFilter] = useState("all");
   //const [fullArtistList, setFullArtistList] = useState(makeList(artists, schedule))
   //navn på array - det er det der er state, navn på funktion - det skal kalde state (rebuilde)
 
@@ -39,7 +30,30 @@ function Artists() {
       async function getSchedule() {
         const resS = await fetch("https://hwaiting.herokuapp.com/schedule");
         const scheduleData = await resS.json();
-        setSchedule(scheduleData);
+
+        const flatArray = [];
+
+        for (const stage in scheduleData) {
+          //console.log(`${stage}: ${scheduleData[stage]}`);
+          //mit array af stages:
+          let stageName = stage;
+
+          //stageData: 3 objekter(stages) med hver 7 objekter som hver har et array med acts
+          const stageData = scheduleData[stageName];
+
+          for (let day in stageData) {
+            //objDay: arrays med acts i (objekter)
+            const dayData = stageData[day];
+
+            for (const obj in dayData) {
+              //bandAct: alle mine bandact objekter
+              const bandAct = dayData[obj];
+              flatArray.push({ ...bandAct, day, stage });
+            }
+          }
+        }
+
+        setSchedule(flatArray);
       }
       getSchedule();
     },
@@ -48,52 +62,18 @@ function Artists() {
     ]
   );
 
-
-let mon = [];
-let tue = [];
-let wed = [];
-let thu = [];
-let fri = [];
-let sat = [];
-let sun = [];
-
-//schedule.Midgard.mon.map;
-//schedule.Vanaheim.mon.map;
-//schedule.Jotunheim.mon.map;
-
-//schedule.Midgard.tue.map;
-//schedule.Vanaheim.tue.map;
-//schedule.Jotunheim.tue.map;
-
- /*  let fullArtistList = artists.map((a) => { MON.INCLUDES(a.name)
-    if(schedule.Midgard.mon.act.includes(a.name) || schedule.Vanaheim.mon.act.includes(a.name) || schedule.Jotunheim.mon.act.includes(a.name)) {
-      return a.day = "Monday"
-    } else if (schedule.Midgard.tue.act.includes(a.name) || schedule.Vanaheim.tue.act.includes(a.name) || schedule.Jotunheim.tue.act.includes(a.name)) {
-      return a.day = "Tuesday"
-    } else if (schedule.Midgard.wed.act.includes(a.name) || schedule.Vanaheim.wed.act.includes(a.name) || schedule.Jotunheim.wed.act.includes(a.name)) {
-      return a.day = "Wednesday"
-    } else if (schedule.Midgard.thu.act.includes(a.name) || schedule.Vanaheim.thu.act.includes(a.name) || schedule.Jotunheim.thu.act.includes(a.name)) {
-      return a.day = "Thursday"
-    } else if (schedule.Midgard.fri.act.includes(a.name) || schedule.Vanaheim.fri.act.includes(a.name) || schedule.Jotunheim.fri.act.includes(a.name)) {
-     return  a.day = "Friday"
-    } else if (schedule.Midgard.sat.act.includes(a.name) || schedule.Vanaheim.sat.act.includes(a.name) || schedule.Jotunheim.sat.act.includes(a.name)) {
-     return  a.day = "Saturday"
-    } else {
-     return a.day = "Sunday"
-    }
-    //a.day = a.name;
-    //return a;
-  });
-
-  console.log("Full List:", fullArtistList) */
   return (
     <main id="artists-main">
       <header id="artists-header">
         <h1>Artists</h1>
         <hr></hr>
       </header>
-      
-      <ArtistList artists={artists}></ArtistList>
+      <ArtistList
+        schedule={schedule}
+        artists={artists}
+        dayFilter={dayFilter}
+        genreFilter={genreFilter}
+      ></ArtistList>
     </main>
   );
 }
