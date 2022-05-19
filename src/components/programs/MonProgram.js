e4eaf4463a35d5477bd682fdf68983cfc6b394f8
Freja <frej400d@stud.kea.dk>
2022-05-19 17:38:23 +0200

    mon mid works

--- src/components/programs/MonProgram.js
+++ src/components/programs/MonProgram.js
@@ -4,37 +4,26 @@ export default function MonProgram({ schedule }) {
   // console.log(schedule);
 
   //new array of all the stages
-  const allStages = schedule.map((el) => ({
+  /*   const allStages = schedule.map((el) => ({
     name: el.stage,
   }));
 
-  const uniStage = [];
+  //gives array with filtered scene names
+  let uniStage = [];
 
-  const cleanStages = allStages.filter((element) => {
-    const isDuplicate = uniStage.includes(element.name);
-
-    if (!isDuplicate) {
+  //gives array with obj name: stage
+  let cleanStages = allStages.filter((element) => {
+    if (!uniStage.includes(element.name)) {
       uniStage.push(element.name);
 
       return true;
     }
-
     return false;
-  });
-
-  // 👇️ [{id: 1, name: 'Tom'}, {id: 2, name: 'Nick'}]
-  console.log(cleanStages);
-  let allStageLists = [];
-  //for each scene make article
-  //for each element(scene) lav et element med array tilsvarende den scene
-  /*   filteredStages.forEach((element) => {
-    allStageLists.push(
-      <article key={element}>
-        {" "}
-        <h3>{element}</h3>{" "}
-      </article>
-    );
   }); */
+
+  /*   let cleanStages = allStages.filter((element) => {
+    if (!uniStage.includes(element.name)) {
+      uniStage.push(element.name); */
 
   //return array or object with scenes
   //om that article make new list
@@ -42,8 +31,9 @@ export default function MonProgram({ schedule }) {
   return (
     <section>
       <h2>Monday 8/8</h2>
-      {/*  {filteredStages.map((band) => {
-        return <MonStage schedule={schedule} />;
+      <MonStage schedule={schedule}></MonStage>
+      {/*      {cleanStages.map((band) => {
+        return <MonStage cleanStages={cleanStages} />;
       })} */}
     </section>
   );
--- src/components/programs/dayStages/MonStages.js
+++ src/components/programs/dayStages/MonStages.js
@@ -1,3 +1,57 @@
 export default function MonStages({ schedule }) {
-  return <div></div>;
+  //console.log(schedule);
+
+  let cleanSchedule = [];
+  //fjerner breaks fra schedule
+  schedule.forEach((e) => {
+    if (e.act !== "break") {
+      cleanSchedule.push(e);
+    }
+  });
+
+  let monMid = [];
+  let monVan = [];
+  let monJon = [];
+
+  let monMidList = [];
+
+  //get my acts on monday on desired stages
+  monMid = cleanSchedule.filter(
+    (act) => act.stage === "Midgard" && act.day === "mon"
+  );
+  monMid.forEach((a) => {
+    monMidList.push(
+      <li>
+        {a.act} {a.start}
+      </li>
+    );
+  });
+
+  console.log(monMidList);
+
+  /*   cleanSchedule.forEach((el) => {
+    if (el.stage === "Midgard" && el.day === "mon") {
+      monMid.push(el);
+    } else if (el.stage === "Vanaheim" && el.day === "mon") {
+      monVan.push(el);
+    } else if (el.stage === "Jotunheim" && el.day === "mon") {
+      monJon.push(el);
+    }
+  });
+
+  let monMidList = [];
+  monMid.forEach((m) => {
+    monMidList.push(<h3 key={m.name}>{m.name}</h3>);
+  });
+  console.log(monMid[0]);
+ */
+
+  return (
+    <>
+      <article>
+        <ul>{monMidList}</ul>
+      </article>
+      <article></article> <article></article>
+    </>
+  );
 }
