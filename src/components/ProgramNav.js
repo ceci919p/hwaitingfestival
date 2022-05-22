ed712f35fabef7fec5effec60b9026a1f93a5c5d
Freja <frej400d@stud.kea.dk>
2022-05-22 15:51:59 +0200

    nav a hover

--- src/components/ProgramNav.js
+++ src/components/ProgramNav.js
@@ -4,37 +4,51 @@ export default function ProgramNav() {
       <ul id="program-nav-container">
         <div className="nav-div"></div>
         <li>
-          <a href="#monday-section">Monday</a>
+          <a id="pro-nav-mon" href="#monday-section">
+            Monday
+          </a>
         </li>
         <div className="nav-div"></div>
         <li>
           {" "}
-          <a href="#tuesday-section">Tuesday</a>
+          <a id="pro-nav-tue" href="#tuesday-section">
+            Tuesday
+          </a>
         </li>
         <div className="nav-div"></div>
         <li>
           {" "}
-          <a href="#wednesday-section">Wednesday</a>
+          <a id="pro-nav-wed" href="#wednesday-section">
+            Wednesday
+          </a>
         </li>
         <div className="nav-div"></div>
         <li>
           {" "}
-          <a href="#thursday-section">Thursday</a>
+          <a id="pro-nav-thu" href="#thursday-section">
+            Thursday
+          </a>
         </li>
         <div className="nav-div"></div>
         <li>
           {" "}
-          <a href="#friday-section">Friday</a>
+          <a id="pro-nav-fri" href="#friday-section">
+            Friday
+          </a>
         </li>
         <div className="nav-div"></div>
         <li>
           {" "}
-          <a href="#saturday-section">Saturday</a>
+          <a id="pro-nav-sat" href="#saturday-section">
+            Saturday
+          </a>
         </li>
         <div className="nav-div"></div>
         <li>
           {" "}
-          <a href="#sunday-section">Sunday</a>
+          <a id="pro-nav-sun" href="#sunday-section">
+            Sunday
+          </a>
         </li>
         <div className="nav-div"></div>
       </ul>
