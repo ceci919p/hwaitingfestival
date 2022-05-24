d1d98764c18160841e4a68b069b4150368e8256d
Freja <frej400d@stud.kea.dk>
2022-05-24 13:37:18 +0200

    day filter fix

--- src/components/artistfilters/DayFilter.js
+++ src/components/artistfilters/DayFilter.js
@@ -15,32 +15,5 @@ export default function DayFilter({ setDayFilter }) {
       <option value="sat">Saturday</option>
       <option value="sun">Sunday</option>
     </select>
-
-    /*     <select id="day-filter" name="day_options">
-      <option onClick={() => setDayFilter("All Days")} value={"All Days"}>
-        All Days
-      </option>
-      <option onClick={() => setDayFilter("mon")} value={"mon"}>
-        Monday
-      </option>
-      <option onClick={() => setDayFilter("tue")} value="tue">
-        Tuesday
-      </option>
-      <option onClick={() => setDayFilter("wed")} value="wed">
-        Wednesday
-      </option>
-      <option onClick={() => setDayFilter("thu")} value="thu">
-        Thursday
-      </option>
-      <option onClick={() => setDayFilter("fri")} value="fri">
-        Friday
-      </option>
-      <option onClick={() => setDayFilter("sat")} value="sat">
-        Saturday
-      </option>
-      <option onClick={() => setDayFilter("sun")} value="sun">
-        Sunday
-      </option>
-    </select> */
   );
 }
