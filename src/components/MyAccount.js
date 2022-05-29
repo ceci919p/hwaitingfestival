0d7f1df76460c3d8dea269ad3fc3a0406ebed760
Freja <frej400d@stud.kea.dk>
2022-05-29 12:13:18 +0200

    info section

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -12,6 +12,18 @@ export default function MyAccount({ fav }) {
         <h1>Profile</h1>
       </header>
       <main>
+        <section>
+          <div>
+            <h2>Favourites</h2>
+            <p>
+              Here you can see your list favourited artists to keep an eye on
+              the artists you definietly don't want to miss!
+            </p>
+            <p>Go to artists</p>
+            <p>Go to program</p>
+          </div>
+          <div></div>
+        </section>
         <section id="fav-list">{favList}</section>
       </main>
       <Footer></Footer>
