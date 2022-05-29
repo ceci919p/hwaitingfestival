a655ae9cea861abbb2514395df08ed66ac74c3f0
Freja <frej400d@stud.kea.dk>
2022-05-29 20:07:28 +0200

    account intro section done

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import FavArtist from "./FavArtist.js";
 import Footer from "../components/Footer";
+import heartImg from "../images/heart.svg";
 
 export default function MyAccount({ fav }) {
   let favList = fav.map((a) => <FavArtist key={a.act} artist={a} />);
@@ -12,17 +14,23 @@ export default function MyAccount({ fav }) {
         <h1>Profile</h1>
       </header>
       <main>
-        <section>
-          <div>
+        <section id="account-info">
+          <div id="account-info-left">
             <h2>Favourites</h2>
             <p>
-              Here you can see your list favourited artists to keep an eye on
+              Here you can see your list of favourited artists to keep an eye on
               the artists you definietly don't want to miss!
             </p>
-            <p>Go to artists</p>
-            <p>Go to program</p>
+            <div id="account-fav-links">
+              <Link id="fav-artist-link" to="/artists">
+                Go to artists 🠮
+              </Link>
+              <Link to="/program">Go to program 🠮</Link>
+            </div>
           </div>
-          <div></div>
+          <div>
+            <img id="account-img" src={heartImg} alt="heart" />
+          </div>
         </section>
         <section id="fav-list">{favList}</section>
       </main>
